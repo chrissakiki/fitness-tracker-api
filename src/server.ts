31bb92db202e00a087859718a32fd1a2584ac416
Chris Akiki <akikichriss@gmail.com>
2026-05-13 22:51:17 +0200

    ADD express server, TS config

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+import express, { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req : Request, res : Response) => {
+  res.send("Fitness Tracker API");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
